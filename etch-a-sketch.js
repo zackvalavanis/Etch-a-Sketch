f2d3b4513c8c70f2a8117ef3c40367b7ef4af04e
Zack <168221270+Zval321@users.noreply.github.com>
2025-05-15 11:47:06 -0500

    the etch a sketch is just about done

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,12 +2,17 @@
 
 const container = document.querySelector('.container-all')
 
-for(let i = 0; i < 16 * 16; i++){ 
-  const cell = document.createElement('div')
-  container.appendChild(cell)
-}
+let gridUnlocked = false
+
+const buttonContainer = document.createElement('div')
+buttonContainer.class = 'button-container'
 
 const button = document.createElement('button')
+button.className = 'button'
+button.textContent = 'Add More Boxes'
+
+buttonContainer.appendChild(button)
+
 button.addEventListener('click', () => { 
   const input = prompt('How many squares per side? (max 100).')
   const size = parseInt(input)
@@ -15,11 +20,29 @@ button.addEventListener('click', () => {
   if(size > 0 && size <= 100){ 
     createGrid(size)
   } else { 
-    alert('Please enter a number between 1 and 100.')
+    alert('Please enter a number between 1 and 1000.')
   }
 })
 
-document.body.insertBefore(button, container)
+
+const divButton = document.createElement('div')
+divButton.className = 'button-container'
+document.body.insertBefore(divButton, container)
+
+
+const buttonRefresh = document.createElement('button')
+buttonRefresh.textContent = 'Refresh'
+buttonRefresh.className = 'button-2'
+
+buttonRefresh.addEventListener('click', () => { 
+  const allBoxes = document.querySelectorAll('.container-all div')
+  allBoxes.forEach((box) => { 
+    box.style.backgroundColor = 'white'
+    gridUnlocked = false
+  })
+})
+
+divButton.append(button, buttonRefresh)
 
 function createGrid(size) { 
   container.innerHTML = ''
@@ -31,6 +54,21 @@ function createGrid(size) {
     cell.style.width = `${squareSize}px`
     cell.style.height = `${squareSize}px`
     cell.style.border = `1px solid black`
+
+    cell.dataset.activated = 'false'
+
+    cell.addEventListener('click', () => {
+      gridUnlocked = !gridUnlocked
+    })
+
+    cell.addEventListener('mouseover', () => {
+      if(gridUnlocked) { 
+        cell.style.backgroundColor = 'black';
+      }
+    });
+
     container.appendChild(cell)
   }
+
 }
+createGrid(16)
